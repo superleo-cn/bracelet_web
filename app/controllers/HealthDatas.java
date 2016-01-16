3c02563807317b834a718a7940190323d4c4287a
superleo <superleo.peking@gmail.com>
2016-01-16 15:13:05 +0800

    Change to fall-down.

--- app/controllers/HealthDatas.java
+++ app/controllers/HealthDatas.java
@@ -5,6 +5,7 @@ import constants.Constants;
 import constants.Messages;
 import models.AnalysisData;
 import models.HealthData;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import play.libs.Json;
@@ -16,6 +17,47 @@ import java.util.List;
 public class HealthDatas extends Controller {
 
     final static Logger logger = LoggerFactory.getLogger(HealthDatas.class);
+
+    // realtime
+    public static Result records() {
+        ObjectNode result = Json.newObject();
+        String braceletId = "1234567";
+        try {
+            result.put(Constants.CODE, Constants.SUCCESS);
+            List<HealthData> datas = HealthData.findRealtimeList(braceletId);
+            StringBuilder sb = new StringBuilder("<table border='1' width='100%'>");
+            sb.append("<tr>");
+            sb.append("<td width='30%'>时间</td>");
+            sb.append("<td width='10%'>体温</td>");
+            sb.append("<td width='10%'>脉搏</td>");
+            sb.append("<td width='10%'>跌倒</td>");
+            sb.append("<td width='10%'>高压</td>");
+            sb.append("<td width='10%'>低压</td>");
+            sb.append("<td width='10%'>经度</td>");
+            sb.append("<td width='10%'>维度</td>");
+            sb.append("</tr>");
+            if(datas != null){
+                datas.stream().forEach(d -> {
+                    sb.append("<tr>");
+                    sb.append("<td>" + DateFormatUtils.format(d.createDate, "yyyy-MM-dd HH:mm:ss") +  "</td>");
+                    sb.append("<td>" + d.temperature + "</td>");
+                    sb.append("<td>" + d.pulseState + "</td>");
+                    sb.append("<td>" + d.motionState + "</td>");
+                    sb.append("<td>" + d.sbp + "</td>");
+                    sb.append("<td>" + d.dbp + "</td>");
+                    sb.append("<td>" + d.latitude + "</td>");
+                    sb.append("<td>" + d.longitude + "</td>");
+                    sb.append("</tr>");
+                });
+            }
+           return ok(sb.toString()).as("text/html");
+        } catch (Exception e) {
+            result.put(Constants.CODE, Constants.ERROR);
+            result.put(Constants.MESSAGE, Messages.HEALTH_DATA_LIST_ERROR);
+            logger.error(Messages.HEALTH_DATA_LIST_ERROR_MESSAGE, new Object[]{braceletId, e});
+        }
+        return ok(result);
+    }
 
     // realtime
     public static Result findRealtimeList(String braceletId, String date, boolean isFirst) {
--- app/models/HealthData.java
+++ app/models/HealthData.java
@@ -27,161 +27,181 @@ import utils.Pagination;
 @Table(name = "tb_health_data")
 public class HealthData {
 
-	final static Logger logger = LoggerFactory.getLogger(HealthData.class);
+    final static Logger logger = LoggerFactory.getLogger(HealthData.class);
 
-	@Id
-	public Long id;
+    @Id
+    public Long id;
 
-	public int motionState = 0;
+    public int motionState = 0;
 
-	public int pulseState = 0;
+    public int pulseState = 0;
 
-	public float temperature = 0.0f;
+    public float temperature = 0.0f;
 
-	public int sbp = 0;
+    public int sbp = 0;
 
-	public int dbp = 0;
+    public int dbp = 0;
 
-	public int warning = 0;
+    public int warning = 0;
 
-	public int archive = 0;
+    public int archive = 0;
 
-	public String braceletId;
+    public Float latitude = 0.0f;
 
-	public Date createDate;
+    public Float longitude = 0.0f;
 
-	// realtime
-	public static List<HealthData> findRealtimeList(String braceletId, String date) {
-		try {
-			ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
-			if (StringUtils.isNotEmpty(braceletId) && StringUtils.isNotEmpty(date)) {
-				Date lastDate = DateUtils.parseDate(date, Constants.PATTERN_YYYYMMDDHHMMSS);
-				Date startDate = DateUtils.addSeconds(lastDate, -5);
-				expList.where().eq("braceletId", braceletId);
-				expList.where().ge("createDate", startDate);
-				expList.where().le("createDate", lastDate);
-			}
-			expList.orderBy("createDate desc");
-			return expList.findList();
-		} catch (Exception e) {
-			logger.error("[findRealtimeList] -> [exception]", e);
-		}
-		return null;
-	}
+    public String braceletId;
 
-	public static List<HealthData> findFirstTime(String braceletId) {
-		try {
-			Pagination pagination = new Pagination();
-			pagination.setPageSize(24);
-			ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
-			PagingList<HealthData> pagingList = expList.findPagingList(pagination.pageSize);
-			pagingList.setFetchAhead(false);
-			expList.orderBy("createDate desc");
-			Page<HealthData> page = pagingList.getPage(pagination.currentPage - 1);
-			return page.getList();
-		} catch (Exception e) {
-			logger.error("[findRealtimeList] -> [exception]", e);
-		}
-		return null;
-	}
+    public Date createDate;
 
-	// history
-	public static List<HealthData> findHistoryList(String braceletId, String type, String date) {
-		try {
-			ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
-			if (StringUtils.isNotEmpty(braceletId) && StringUtils.isNotEmpty(date)) {
-				List<Date> days = null;
-				if (StringUtils.equals(type, "day")) {
-					days = MyDateUtils.getDateByDay();
-				} else if (StringUtils.equals(type, "week")) {
-					days = MyDateUtils.getDateByWeek();
-				} else if (StringUtils.equals(type, "month")) {
-					days = MyDateUtils.getDateByMonth();
-				} else {
-					days = MyDateUtils.getDateByDay();
-				}
-				expList.where().ge("createDate", days.get(0));
-				expList.where().le("createDate", days.get(1));
-				expList.where().eq("braceletId", braceletId);
-			}
-			expList.orderBy("createDate desc");
-			return expList.findList();
-		} catch (Exception e) {
-			logger.error("[findHistoryList] -> [exception]", e);
-		}
-		return null;
-	}
+    // test only
+    public static List<HealthData> findRealtimeList(String braceletId) {
+        try {
+            ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
+            if (StringUtils.isNotEmpty(braceletId)) {
+                expList.where().eq("braceletId", braceletId);
+                expList.findPagingList(50);
+            }
+            expList.orderBy("createDate desc");
+            return expList.findList();
+        } catch (Exception e) {
+            logger.error("[findRealtimeList] -> [exception]", e);
+        }
+        return null;
+    }
 
-	// interface
-	public static List<HealthData> findList(String braceletId, String date) {
-		try {
-			ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
-			if (StringUtils.isNotEmpty(braceletId) && StringUtils.isNotEmpty(date)) {
-				Date startDate = DateUtils.parseDate(date, Constants.PATTERN_YYYYMMDD);
-				Date endDate = DateUtils.addDays(startDate, 1);
-				expList.where().eq("braceletId", braceletId);
-				expList.where().ge("createDate", startDate);
-				expList.where().le("createDate", endDate);
-			}
-			expList.orderBy("createDate desc");
-			return expList.findList();
-		} catch (Exception e) {
-			logger.error("[findList] -> [exception]", e);
-		}
-		return null;
-	}
+    // realtime
+    public static List<HealthData> findRealtimeList(String braceletId, String date) {
+        try {
+            ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
+            if (StringUtils.isNotEmpty(braceletId) && StringUtils.isNotEmpty(date)) {
+                Date lastDate = DateUtils.parseDate(date, Constants.PATTERN_YYYYMMDDHHMMSS);
+                Date startDate = DateUtils.addSeconds(lastDate, -5);
+                expList.where().eq("braceletId", braceletId);
+                expList.where().ge("createDate", startDate);
+                expList.where().le("createDate", lastDate);
+            }
+            expList.orderBy("createDate desc");
+            return expList.findList();
+        } catch (Exception e) {
+            logger.error("[findRealtimeList] -> [exception]", e);
+        }
+        return null;
+    }
 
-	// interface
-	public static HealthData findLatest(String braceletId) {
-		try {
-			ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
-			if (StringUtils.isNotEmpty(braceletId)) {
-				PagingList<HealthData> pagingList = expList.findPagingList(1);
-				expList.where().eq("braceletId", braceletId);
-				pagingList.setFetchAhead(false);
-				expList.orderBy("createDate desc");
-				Page<HealthData> page = pagingList.getPage(0);
-				List<HealthData> list = page.getList();
-				if (CollectionUtils.isNotEmpty(list)) {
-					return list.get(0);
-				}
-			}
-		} catch (Exception e) {
-			logger.error("[findLatest] -> [exception]", e);
-		}
-		return null;
-	}
+    public static List<HealthData> findFirstTime(String braceletId) {
+        try {
+            Pagination pagination = new Pagination();
+            pagination.setPageSize(24);
+            ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
+            PagingList<HealthData> pagingList = expList.findPagingList(pagination.pageSize);
+            pagingList.setFetchAhead(false);
+            expList.orderBy("createDate desc");
+            Page<HealthData> page = pagingList.getPage(pagination.currentPage - 1);
+            return page.getList();
+        } catch (Exception e) {
+            logger.error("[findRealtimeList] -> [exception]", e);
+        }
+        return null;
+    }
 
-	public static List<HealthData> findUrgentList(String braceletId) {
-		try {
-			ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
-			if (StringUtils.isNotEmpty(braceletId)) {
-				expList.where().eq("braceletId", braceletId);
-				expList.where().eq("warning", 1);
-				expList.where().eq("archive", 0);
-			}
-			expList.orderBy("createDate desc");
-			return expList.findList();
-		} catch (Exception e) {
-			logger.error("[findUrgentList] -> [exception]", e);
-		}
-		return null;
-	}
+    // history
+    public static List<HealthData> findHistoryList(String braceletId, String type, String date) {
+        try {
+            ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
+            if (StringUtils.isNotEmpty(braceletId) && StringUtils.isNotEmpty(date)) {
+                List<Date> days = null;
+                if (StringUtils.equals(type, "day")) {
+                    days = MyDateUtils.getDateByDay();
+                } else if (StringUtils.equals(type, "week")) {
+                    days = MyDateUtils.getDateByWeek();
+                } else if (StringUtils.equals(type, "month")) {
+                    days = MyDateUtils.getDateByMonth();
+                } else {
+                    days = MyDateUtils.getDateByDay();
+                }
+                expList.where().ge("createDate", days.get(0));
+                expList.where().le("createDate", days.get(1));
+                expList.where().eq("braceletId", braceletId);
+            }
+            expList.orderBy("createDate desc");
+            return expList.findList();
+        } catch (Exception e) {
+            logger.error("[findHistoryList] -> [exception]", e);
+        }
+        return null;
+    }
 
-	public static void updateUrgentList(List<Long> ids) {
-		try {
-			if (ids != null) {
-				for (Long id : ids) {
-					HealthData data = Ebean.find(HealthData.class, id);
-					if (data != null) {
-						data.archive = 1;
-						Ebean.update(data);
-					}
-				}
-			}
-		} catch (Exception e) {
-			logger.error("[updateUrgentList] -> [exception]", e);
-		}
-	}
+    // interface
+    public static List<HealthData> findList(String braceletId, String date) {
+        try {
+            ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
+            if (StringUtils.isNotEmpty(braceletId) && StringUtils.isNotEmpty(date)) {
+                Date startDate = DateUtils.parseDate(date, Constants.PATTERN_YYYYMMDD);
+                Date endDate = DateUtils.addDays(startDate, 1);
+                expList.where().eq("braceletId", braceletId);
+                expList.where().ge("createDate", startDate);
+                expList.where().le("createDate", endDate);
+            }
+            expList.orderBy("createDate desc");
+            return expList.findList();
+        } catch (Exception e) {
+            logger.error("[findList] -> [exception]", e);
+        }
+        return null;
+    }
+
+    // interface
+    public static HealthData findLatest(String braceletId) {
+        try {
+            ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
+            if (StringUtils.isNotEmpty(braceletId)) {
+                PagingList<HealthData> pagingList = expList.findPagingList(1);
+                expList.where().eq("braceletId", braceletId);
+                pagingList.setFetchAhead(false);
+                expList.orderBy("createDate desc");
+                Page<HealthData> page = pagingList.getPage(0);
+                List<HealthData> list = page.getList();
+                if (CollectionUtils.isNotEmpty(list)) {
+                    return list.get(0);
+                }
+            }
+        } catch (Exception e) {
+            logger.error("[findLatest] -> [exception]", e);
+        }
+        return null;
+    }
+
+    public static List<HealthData> findUrgentList(String braceletId) {
+        try {
+            ExpressionList<HealthData> expList = Ebean.find(HealthData.class).where();
+            if (StringUtils.isNotEmpty(braceletId)) {
+                expList.where().eq("braceletId", braceletId);
+                expList.where().eq("warning", 1);
+                expList.where().eq("archive", 0);
+            }
+            expList.orderBy("createDate desc");
+            return expList.findList();
+        } catch (Exception e) {
+            logger.error("[findUrgentList] -> [exception]", e);
+        }
+        return null;
+    }
+
+    public static void updateUrgentList(List<Long> ids) {
+        try {
+            if (ids != null) {
+                for (Long id : ids) {
+                    HealthData data = Ebean.find(HealthData.class, id);
+                    if (data != null) {
+                        data.archive = 1;
+                        Ebean.update(data);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("[updateUrgentList] -> [exception]", e);
+        }
+    }
 
 }
